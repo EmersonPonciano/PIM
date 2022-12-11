ce10912a715cdc2fdc80d40ae548538b182b3869
Emerson Ponciano <emersonponciano2@gmail.com>
2022-12-10 23:03:29 -0300

    login e cadastro de usuários com validação prontos

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,31 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css';
-import { Link} from "react-router-dom"
+import {auth} from '../../config/firebase';
+import { toast } from 'react-toastify';
+import { Link, useHistory } from "react-router-dom";
 
 const Login = () => {
+
+  const[email, setEmail]= useState("");
+  const[senha, setSenha]= useState("");
+
+  const navigate = useHistory();
+
+  const logar = async () => {
+    try {
+    const user = await auth.signInWithEmailAndPassword(email, senha);
+    navigate.push("/home");
+    console.log(user);
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  function handleLogin(e){
+    e.preventDefault();
+    if (!email || !senha) {
+      toast.error("Campos em branco");
+    }else{
+      logar();
+    }
+}
+
   return (
     <div className="login">
       <h1 className="loginName">Login</h1>
       <form
         className="login__form"
+        onSubmit={handleLogin}
       >
         <label>Email</label>
         <input
           type="email"
           placeholder=""
+         onChange={(event) => {setEmail(event.target.value)}}
         />
 
         <label>Senha</label>
         <input
           type="password"
           placeholder=""
+          onChange={(event) => {setSenha(event.target.value)}}
         />
 
         <div className="cadastro-link">
           <Link to="./Signup" className="textoConta">Não tem uma conta? <span>Cadastre-se.</span></Link>
         </div>
 
-        <Link to="./Home"><input type="submit" value="Login"/></Link>
+        <input type="submit" value="Login"/>
 
       </form>
     </div>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,34 +1,48 @@
 import React, { useState } from "react";
-import { Link} from "react-router-dom";
 import './style.css';
+import { toast } from 'react-toastify';
 import {auth} from '../../config/firebase';
+import { useHistory } from "react-router-dom";
+
 
 const Signup = () => {
 
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
+  const navigate = useHistory();
+
   const registrar = async () => {
     try {
     const user = await auth.createUserWithEmailAndPassword(email, senha);
+    navigate.push("/home");
     console.log(user);
     } catch (error) {
       console.log(error.message)
     }
   }
 
+  function handleRegister(e){
+    e.preventDefault();
+    if (!email || !senha) {
+      toast.error("Campos em branco");
+    }else{
+      registrar();
+    }
+}
+
   return (
     <div className="signup">
       <h1 className="loginName">Cadastro</h1>
       <form
         className="signup__form"
-        onSubmit={registrar}
+        onSubmit={handleRegister}
       >
         <label>Email</label>
         <input
           type="email"
           placeholder=""
-          onChange={(event) => {setEmail(event.target.value)}}
+          onChange={(event) => {setEmail(event.target.value)}}    
         />
 
         <label>Senha</label>
